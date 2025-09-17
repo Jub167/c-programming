f47dba695a666d9997dc37c4a8ecd9b97a06886f
Jub167 <sameerdunge0@gmail.com>
2025-09-17 15:00:59 +0530

    Add files via upload

---
+++ practical 7.1b/call by reference/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swapreference (int *x, int *y) {
+    int  temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("inside swapreference - x: %d, y: %d\n", *x, *y);
+}
+
+int main()
+{ int a, b;
+
+printf("enter two numbers : ");
+scanf("%d %d", &a, &b);
+
+printf("before swapreference - a: %d, b: %d\n", a, b);
+swapreference(&a, &b);
+
+printf("after swapreference - a: %d, b: %d\n", a, b);
+
+
+    return 0;
+}
